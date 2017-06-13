87f53aad9966aa736fb4f4ba1c9de3a4f5b52065
Angel Ortega <an22ortega@gmail.com>
2017-06-12 19:37:31 -0700

    tuple

--- boostit.py
+++ boostit.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python2.7
 import sys
+from collections import namedtuple
 
-class Context():
+TrainingFile = namedtuple("TrainingFile",["pos","neg"])
+TestingFile = namedtuple("TestingFile", ["pos", "neg"])
+
+
+class Context:
     def __init__(self, usage):
         if len(usage) != 6:
-            print 'Error: usage is <ensemble size> <pos-train-file> <neg-train-file> <pos-test-file> <neg-test-file>\n\nGiven input:\t{0}'.format(usage)
-        self._ensemble_size = usage[1]
-        self.training_files = [usage[2], usage[3]]
-        self.testing_files = [usage[4], usage[5]]
-                 #setattr(self, name, value)
+            print("Error: usage is <ensemble size> <pos-train-file> "
+                  "<neg-train-file> <pos-test-file> <neg-test-file>\n\nGiven input:\t{0}".format(usage))
+            sys.exit(1)
+
+        self.ensemble_size = usage[1]
+        self.training_files = TrainingFile(usage[2], usage[3])
+        self.testing_files = TestingFile(usage[4], usage[5])
 
 
 if __name__ == '__main__':
