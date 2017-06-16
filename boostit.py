040aff91f1c002c62343442f846c76c7f55990a3
Angel Ortega <an22ortega@gmail.com>
2017-06-15 19:17:59 -0700

    loading data to their partitions

--- boostit.py
+++ boostit.py
@@ -8,26 +8,23 @@ import numpy as np
 from math import log
 from cmath import log
 
+import re
+
 TrainingFile = namedtuple("TrainingFile",["pos","neg"])
 TestingFile = namedtuple("TestingFile", ["pos", "neg"])
+# 𝓓 = namedtuple("𝓓", ["pos", "neg"])
+
+# 𝓓Training
+
+# Python 3.0+
+# from abc import ABCMeta, abstractmethod
+# class Abstract(metaclass=ABCMeta):
+#     @abstractmethod
+#     def foo(self):
+#         pass
 
 
 class EnsembleMethod(object):
-
-    def load_data(training_file):
-        '''
-        Transform a data file into a np ndarray with any N features.
-        The first column is ignored because the record number does not add any values as a feature
-        :param training_file:
-        :return:
-        '''
-        with open(training_file) as file:
-            num_points, point_dimensionality = map(int, re.split('\s+', file.readline().strip()))
-        file.close()
-
-        # Data is (n_samples, n_features)
-        data = np.loadtxt(training_file, skiprows=1, usecols=range(0, point_dimensionality))
-        return data, num_points, point_dimensionality
 
     class Context:
         def __init__(self, usage):
@@ -39,6 +36,70 @@ class EnsembleMethod(object):
             self.ensemble_size = usage[1]  # ensemble_size
             self.training_files = TrainingFile(usage[2], usage[3])
             self.testing_files = TestingFile(usage[4], usage[5])
+            self.pos_training_file = usage[2]
+            self.neg_training_file = usage[3]
+            self.pos_testing_file = usage[4]
+            self.neg_testing_file = usage[5]
+            self.output_filename = self.interpret_output_filename(self.pos_training_file)
+
+            # Get predictors, also known as x_vector or feature_vector
+            self.pos_training_predictors, self.pos_num_training_samples, self.pos_training_dimensionality = self.load_data(
+                self.pos_training_file)
+            self.neg_training_predictors, self.neg_num_training_samples, self.neg_training_dimensionality = self.load_data(
+                self.neg_training_file)
+            training_predictors = np.concatenate((self.pos_training_predictors, self.neg_training_predictors), axis=0)
+
+            self.pos_testing_predictors, self.pos_num_testing_samples, self.pos_testing_dimensionality = self.load_data(self.pos_testing_file)
+            self.neg_testing_predictors, self.neg_num_testing_samples, self.neg_testing_dimensionality = self.load_data(self.neg_testing_file)
+            self.testing_predictors = np.concatenate((self.pos_testing_predictors, self.neg_testing_predictors), axis=0)
+
+            # Get targets, also known as Y, actual, or labels
+            self.pos_training_targets = np.ones(self.pos_num_training_samples, dtype=int)
+            self.neg_training_targets = np.full(self.neg_num_training_samples, fill_value=-1, dtype=int)
+            # y_training_targets = np.zeros(pos_num_training_samples + neg_num_training_samples, dtype=int)
+            self.y_training_targets = np.concatenate((self.pos_training_targets, self.neg_training_targets))
+
+            self.pos_testing_targets = np.ones(self.pos_num_testing_samples, dtype=int)
+            self.neg_testing_targets = np.full(self.neg_num_testing_samples, fill_value=-1, dtype=int)
+            self.y_testing_targets = np.concatenate((self.pos_testing_targets, self.neg_testing_targets))
+            self.ground_truth_targets = map(lambda y: 0 if y == -1 else 1, self.y_testing_targets)
+
+            self.num_training = len(self.y_training_targets)
+            self.num_testing = len(self.y_testing_targets)
+
+            # clf_model = PerceptronModel(training_predictors, y_training_targets, num_training,
+            #                             kernelmodel=('rbf', sigma))
+            # similarities = clf_model.parameterize_RBF(training_predictors, training_predictors)
+            # clf_model.converge_training_weights(clf_model.alphas)
+            # predicted = clf_model.predict(testing_predictors, y_testing_targets)
+            #
+            # alphas_list = ' '.join('{0}'.format(v, i) for i, v in enumerate(clf_model.alphas))
+            # print
+            # 'Alphas:\t' + alphas_list
+
+        def interpret_output_filename(self, file_with_digits):
+            try:
+                file_no = int(re.match('.+([0-9]+)[^0-9]*$', file_with_digits).group(1))
+                outputfile = 'output{0}.txt'.format(file_no)
+            except (ValueError, AttributeError) as e:
+                outputfile = 'output.txt'
+            return outputfile
+
+
+        def load_data(self, training_file):
+            '''
+            Transform a data file into a np ndarray with any N features.
+            The first column is ignored because the record number does not add any values as a feature
+            :param training_file:
+            :return:
+            '''
+            with open(training_file) as file:
+                num_points, point_dimensionality = map(int, re.split('\s+', file.readline().strip()))
+            file.close()
+
+            # Data is (n_samples, n_features)
+            data = np.loadtxt(training_file, skiprows=1, usecols=range(0, point_dimensionality))
+            return data, num_points, point_dimensionality
 
     class BoostIt:
         def __init__(self, context):
@@ -48,6 +109,8 @@ class EnsembleMethod(object):
             self.𝓓 = [context.training_files, context.testing_files]
             # self.cardinality_D =
             self.𝒜 = 'triclassify'
+            # EnsembleMethod.load_data(self.𝓓.training_file)
+
 
         def boosting(self, 𝓓, 𝛵, 𝒜):
             w⃗ = cardinality_D * [float(1)/2]
@@ -113,3 +176,6 @@ if __name__ == '__main__':
 
 
     # ctx = Context(sys.argv)
+
+
+# /home/angel/Courses/cs165b/machine-learning-cs165b-assignment5/boostit.py
